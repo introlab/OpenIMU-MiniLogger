2adb87ce134d71b83d02e49246ca82a7e844f9fb
Cédric Godin <cedric.godin@usherbrooke.ca>
2019-07-09 14:04:16 -0400

    Add license

--- OLED-Test-Firmware/src/display.cpp
+++ OLED-Test-Firmware/src/display.cpp
@@ -1,3 +1,24 @@
+/*
+ * Display module for Open IMU
+ * author: Cedric Godin
+ * 
+ * Copyright 2018 IntRoLab
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "display.h"
 #include <string>
 #include <sstream>
@@ -71,9 +92,12 @@ void Display::clear()
 }
 
 
-// display sleep screen
+// Paint state
 void Display::displayVoltage(float volts, float current,bool validData, bool stateLog, bool sdLog)
 {
+    /* volts: battery voltage
+    */
+
     SSD1331_clear();
 
     std::stringstream batt_text;
